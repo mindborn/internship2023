e721647cd6fff6f005c5117afee91d6424be3c77
Manoj Bhadugale <mindbornsoftwaresolutions@gmail.com>
2023-04-14 11:48:08 +0530

    Full stack

--- spring/jdbcrest/src/main/java/in/mindborn/jdbcrest/JdbcrestApplication.java
+++ spring/jdbcrest/src/main/java/in/mindborn/jdbcrest/JdbcrestApplication.java
@@ -2,9 +2,27 @@ package in.mindborn.jdbcrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JdbcrestApplication {
+
+	@Bean
+    public WebMvcConfigurer configurer()
+    {
+        return new WebMvcConfigurer()
+        {
+            @Override
+            public void addCorsMappings(CorsRegistry registry)
+            {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcrestApplication.class, args);
